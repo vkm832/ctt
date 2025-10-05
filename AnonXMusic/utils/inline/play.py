24158b7e9dad85199957236c26cd5bc5c550b15b
Vk <s10517218@gmail.com>
2025-10-05 19:51:09 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -65,7 +65,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
+        ],    [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")],
     ]
     
     return buttons
@@ -79,7 +79,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
+        ],    [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")],
     ]
     return buttons
 
