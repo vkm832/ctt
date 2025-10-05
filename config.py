5ef73c8c68104e2b045f5d1cee01b5dd9287d40a
Vk <s10517218@gmail.com>
2025-10-05 19:54:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/DukaanTelegramKi",
+    "https://github.com/Vivek12-0/s",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
