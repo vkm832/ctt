25c25785b63d58f88992abfdcf54d7f60f33ca12
vkm832 <bot627668@gmail.com>
2025-10-31 02:53:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ChutkiUpdate")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ChutkiSupport")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 ASSISTANT_LEAVE_TIME = int(getenv("ASSISTANT_LEAVE_TIME",  5400))
 
 
